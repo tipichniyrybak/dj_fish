c6e6a461e18c1f0e8ea745ce7ab1970351f75e95
nikifor511 <deadnikifor@gmail.com>
2020-01-16 11:34:57 +0300

    xx

--- fish_pr/fish_pr/apps/fish_app/views.py
+++ fish_pr/fish_pr/apps/fish_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import FishingPlace, Order
 from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     places = FishingPlace.objects.order_by('-id')
@@ -11,7 +12,17 @@ def detail(request, place_id):
         p = FishingPlace.objects.get( id=place_id)
     except:
         raise Http404("Данные не найдены!.." )
-    return render(request, 'fish_app/detail.html', {'place': p})
+
+    latest_order_list = p.order_set.order_by('-id')[:10]
+
+    return render(request, 'fish_app/detail.html', {'place': p, 'latest_order_list': latest_order_list})
 
 def leave_comment(request, place_id):
-    pass
+    try:
+        p = FishingPlace.objects.get(id=place_id)
+    except:
+        raise Http404("Данные не найдены!..")
+
+    p.order_set.create(user_id=2, description=request.POST['description'], photos="pic.jpg")
+    return HttpResponseRedirect(reverse('fish_app:detail', args=(p.id,)))
+
