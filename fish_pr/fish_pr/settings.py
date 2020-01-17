7077e04a3ab2ab03d1bf7cfc63240904a7f21a5e
nikifor511 <deadnikifor@gmail.com>
2020-01-17 12:49:58 +0300

    xx

--- fish_pr/fish_pr/settings.py
+++ fish_pr/fish_pr/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'workspace',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -41,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
